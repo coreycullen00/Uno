a61678519fdf0e1db98ea8a8b1b19f28e73b9105
Corey Cullen <coreycullen00@live.ie>
2023-07-20 17:20:18 +0100

    setup

---
+++ public/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="header">
+        Uno
+      </header>
+    </div>
+  );
+}
+
+export default App;
