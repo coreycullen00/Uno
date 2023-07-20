d3f9830d3548a8c441e44c9926a1ed320997949b
Corey Cullen <coreycullen00@live.ie>
2023-07-20 18:37:14 +0100

    environment tidy up

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="header">
-        Uno
+        Chat App
       </header>
     </div>
   );
