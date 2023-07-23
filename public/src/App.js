95e65838841c40601ec3b001c721378bdd430d97
corey <coreycullen00@live.ie>
2023-07-23 13:43:22 +0100

    routes

--- public/src/App.js
+++ public/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="header">
-        Chat App
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
